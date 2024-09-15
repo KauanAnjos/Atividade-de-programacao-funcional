c267df791f91858a3c0761caf255a26b887b08c7
KauanAnjos <kauandosanjosmeneses@gmail.com>
2024-09-15 20:24:46 -0300

    Integrando os o javascript com o html

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ function decideGanhador(jogador1, jogador2, regraVencedor) {
 }  
 
 const regra = (jogador1, jogador2) => {
-    if (jogador1 === jogador2) return
-    "Ninguém ganhou! Deu empate :)"
+    if (jogador1 === jogador2) return "Ninguém ganhou! Deu empate :)"
     if ((jogador1 === "pedra" && jogador2 === "tesoura") || 
         (jogador1 === "tesoura" && jogador2 === "papel") ||
         (jogador1 === "papel" && jogador2 === "pedra")) {
@@ -23,9 +22,9 @@ function criarPlacar() {
     let jogador2Vitorias = 0
 
     return function(vencedor) {
-        if (vencedor === "Parabéns! Jogador 1 venceu essa rodada!!!") {
+        if (vencedor === "Parabéns! Você venceu essa rodada!!!") {
             jogador1Vitorias++
-        } else if (vencedor === "Parabéns! Jogador 2 venceu essa rodada!!!") {
+        } else if (vencedor === "Parabéns! o Jogador 2 venceu essa rodada!!!") {
             jogador2Vitorias++
         }
         return {jogador1Vitorias, jogador2Vitorias}
@@ -35,26 +34,27 @@ function criarPlacar() {
 const atualizarPlacar = criarPlacar()
 
 function jogar() {
-        const jogador1Escolha = prompt("Vamos Jogar? Escolha Pedra, paapel ou tesoura (ou 'sair' para finalizar):").trim()
-        if (jogador1Escolha.toLowerCase() === 'sair') {
+        const jogador1Escolha = document.getElementById('escolhaJogador1').value.toLowerCase().trim()
+        console.log(jogador1Escolha)
+
+        if (jogador1Escolha.toLowerCase() === 'sair') {  // Não precisamos mais
             return console.log("Jogo finalizado. Bye bye :)")
         }
+        
+        if (!listaJogadas.includes(jogador1Escolha)) {
+            alert("hm, parece que algo deu errado. Digite novamente.")
+        }
+        
+        const jogador2Escolha = listaJogadas[Math.floor(Math.random() * listaJogadas.length)]
+        document.getElementById('escolhaJogador2').innerHTML = `${jogador2Escolha}`
+        console.log(`O jogador 2 escolheu ${jogador2Escolha}`)
+        
+        const vencedor = decideGanhador(jogador1Escolha, jogador2Escolha, regra)
+        document.getElementById('resultado').innerHTML = `${vencedor}`
+        console.log(`ATENCION PARA O RESULTADO: ${vencedor}`)
+        
+        const {jogador1Vitorias, jogador2Vitorias} = atualizarPlacar(vencedor)   
+        document.getElementById('pontosjogador1').innerHTML = `${jogador1Vitorias}`
+        document.getElementById('pontosjogador2').innerHTML = `${jogador2Vitorias}`
+        console.log(`Placar: Jogador 1 ${jogador1Vitorias}, Jogador 2: ${jogador2Vitorias}`)
     }
-
-const padronizandoEscolha = jogador1Escolha.charAt(0).toUpperCase() + jogador1Vitorias.slice(1).toLowerCase()
-
-if (!listaJogadas.includes(padronizandoEscolha)) {
-    console.log("hm, parece que algo deu errado. Escolha novamente.")
-    jogar()
-}
-
-const jogador2Escolha = listaJogadas[Math.floor(Math.random() * listaJogadas.length)]
-console.log("O jogador 2 escolheu ${jogador2Escolha}")
-
-const vencedor = ganhador(padronizandoEscolha, jogador2Escolha, regra)
-console.log("ATENCION PARA O RESULTADO: ${vencedor}")
-
-const {jogador1Vitorias, jogador2Vitorias} = atualizarPlacar(vencedor)
-console.log("Placar: Jogador 1 ${jogador1Vitorias}, Jogador 2: ${jogador2Vitorias}\n")
-
-jogar()
