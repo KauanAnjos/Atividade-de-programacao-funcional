f861978c4199db19cc46156a6e99d31367f5fba7
KauanAnjos <kauandosanjosmeneses@gmail.com>
2024-09-15 22:51:48 -0300

    ajustes

--- main.js
+++ main.js
@@ -8,11 +8,10 @@ function decideGanhador(jogador1, jogador2, regraVencedor) {
 
 const regra = (jogador1, jogador2) => {
     if (jogador1 === jogador2) return "Ninguém ganhou! Deu empate :)"
-    console.log(jogador1, jogador2)
     if ((jogador1 === "pedra" && jogador2 === "tesoura") || 
         (jogador1 === "tesoura" && jogador2 === "papel") ||
         (jogador1 === "papel" && jogador2 === "pedra")) {
-            return "Parabéns! Você venceu essa rodada!!!"
+            return "Parabéns! Jogador 1 venceu essa rodada!!!"
         } else {
             return "Parabéns! Jogador 2 essa rodada venceu!!!"
         }
@@ -28,7 +27,6 @@ function criarPlacar() {
         } else if (vencedor === "Parabéns! Jogador 2 essa rodada venceu!!!") {
             jogador2Vitorias++
         }
-        console.log(jogador1Vitorias, jogador2Vitorias)
         return {jogador1Vitorias, jogador2Vitorias}
     }
 }
@@ -37,7 +35,7 @@ const atualizarPlacar = criarPlacar()
 
 function jogar() {
         const jogador1Escolha = document.getElementById('escolhaJogador1').value.toLowerCase().trim()
-        console.log(jogador1Escolha)
+        //console.log(`O jogador 1 escolheu ${jogador1Escolha}`)
 
         if (jogador1Escolha.toLowerCase() === 'sair') {  // Não precisamos mais
             return console.log("Jogo finalizado. Bye bye :)")
@@ -45,17 +43,18 @@ function jogar() {
         
         if (!listaJogadas.includes(jogador1Escolha)) {
             alert("hm, parece que algo deu errado. Digite novamente.")
+            return
         }
         
         const jogador2Escolha = listaJogadas[Math.floor(Math.random() * listaJogadas.length)]
+        //console.log(`O jogador 2 escolheu ${jogador2Escolha}`)
         document.getElementById('escolhaJogador2').innerHTML = `${jogador2Escolha}`
-        console.log(`O jogador 2 escolheu ${jogador2Escolha}`)
         
         const vencedor = decideGanhador(jogador1Escolha, jogador2Escolha, regra)
+        //console.log(`ATENCION PARA O RESULTADO: ${vencedor}`)
         document.getElementById('resultado').innerHTML = `${vencedor}`
-        console.log(`ATENCION PARA O RESULTADO: ${vencedor}`)
         
         const {jogador1Vitorias, jogador2Vitorias} = atualizarPlacar(vencedor)   
-        console.log(`Placar: Jogador 1 ${jogador1Vitorias}, Jogador 2: ${jogador2Vitorias}`)
+       // console.log(`Placar: Jogador 1 ${jogador1Vitorias}, Jogador 2: ${jogador2Vitorias}`)
         document.getElementById('placar').textContent = `${jogador1Vitorias} : ${jogador2Vitorias}`
     }
